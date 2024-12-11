1d17661033afaf448225bd517d85a9dbf200ea51
Paul Canham <paul@canham.io>
2024-12-11 08:44:37 +0000

    Create update-readme.js

---
+++ scripts/update-readme.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const axios = require('axios');
+
+async function getRepos() {
+  const response = await axios.get('https://api.github.com/user/repos', {
+    headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` }
+  });
+  return response.data;
+}
+
+async function updateReadme() {
+  const repos = await getRepos();
+  const technologies = new Set();
+
+  repos.forEach(repo => {
+    if (repo.language) {
+      technologies.add(repo.language);
+    }
+  });
+
+  const badges = Array.from(technologies).map(tech => `!${tech}`).join(' ');
+
+  let readmeContent = fs.readFileSync('README.md', 'utf8');
+  readmeContent = readmeContent.replace(/<!-- TECHNOLOGIES:START -->[\s\S]*<!-- TECHNOLOGIES:END -->/, `<!-- TECHNOLOGIES:START -->\n${badges}\n<!-- TECHNOLOGIES:END -->`);
+
+  fs.writeFileSync('README.md', readmeContent);
+}
+
+updateReadme();
